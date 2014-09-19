b44cbfa01a9e7c812e371754894f57b2e8ea2db1
unknown <C097095@FW3BCCNU3339B89.AP.LILLY.com>
2014-09-19 16:01:09 +0800

    Completed R Programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -25,7 +25,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 #The following function solves for the inverse of the special "matrix" created with the above function. However, it first
-#checks to see if the inverse has already been calculated. If so, it gets the inverse from the cache and skips the #computation. Otherwise, it solves for the inverse of the matrix and sets the value of the inverse in the cache via the #setinverse function.
+#checks to see if the inverse has already been calculated. If so, it gets the inverse from the cache and skips the
+#computation. Otherwise, it solves for the inverse of the matrix and sets the value of the inverse in the cache via the
+#setinverse function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
